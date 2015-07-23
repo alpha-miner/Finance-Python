12e1ad22c290047995589e418fd4ce75e459daf0
Cheng.Li <cheng.li@datayes.com>
2015-07-23 18:15:50 +0800

    Added missing package

--- finpy/__init__.py
+++ finpy/__init__.py
@@ -5,10 +5,15 @@ Created on 2015-7-9
 @author: cheng.li
 """
 
-__all__ = ['__version__', 'api', 'DateUtilities', 'Enums']
+__all__ = ['__version__', 'api', 'DateUtilities', 'Enums', 'Env', 'Math', 'PricingEngines', 'Risk']
 
 __version__ = "0.2.0"
 
 import finpy.api as api
 import finpy.DateUtilities as DateUtilities
 import finpy.Enums as Enums
+import finpy.Env as Env
+import finpy.Math as Math
+import finpy.PricingEngines as PricingEngines
+import finpy.Risk as Risk
+

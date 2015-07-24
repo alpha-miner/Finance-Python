ae2fce5cf630faeb340739835e6cef819cc51775
Cheng.Li <cheng.li@datayes.com>
2015-07-24 13:36:04 +0800

    CTD

--- finpy/PricingEngines/BlackFormula.py
+++ finpy/PricingEngines/BlackFormula.py
@@ -8,6 +8,7 @@ Created on 2015-7-23
 from finpy.Enums.OptionType import OptionType
 from finpy.Math.Distributions.NormalDistribution import CumulativeNormalDistribution
 from finpy.Math.MathConstants import _M_PI
+from finpy.Math.MathConstants import _QL_EPSILON
 import math
 from scipy.optimize import newton
 
@@ -27,13 +28,13 @@ def _checkParameters(strike, forward, displacement):
 def blackFormula(optionType,
                  strike,
                  forward,
-                 stdev,
+                 stdDev,
                  discount=1.0,
                  displacement=0.0):
 
     strike, forward, displacement = _checkParameters(strike, forward, displacement)
 
-    if stdev == 0.0:
+    if stdDev == 0.0:
         return max((forward - strike) * optionType, 0.0) * discount
 
     forward += displacement
@@ -42,8 +43,8 @@ def blackFormula(optionType,
     if strike == 0.0:
         return forward * discount if optionType == OptionType.Call else 0.0
 
-    d1 = math.log(forward / strike) / stdev + 0.5 * stdev
-    d2 = d1 - stdev
+    d1 = math.log(forward / strike) / stdDev + 0.5 * stdDev
+    d2 = d1 - stdDev
     nd1 = _dist(d1 * optionType)
     nd2 = _dist(d2 * optionType)
 
@@ -115,6 +116,104 @@ def blackFormulaImpliedStdDev(optionType,
     assert stdAppr >= 0.0, "stdDev ({0:f})) must be non-negative".format(stdAppr)
     return newton(func, stdAppr, tol=1e-10)
 
+
+def bachelierFormula(optionType,
+                     strike,
+                     forward,
+                     stdDev,
+                     discount=1.0):
+
+    strike = float(strike)
+    forwoard = float(forward)
+    stdDev = float(stdDev)
+    discount = float(discount)
+
+    assert stdDev >= 0, "stdDev ({0:f}) must be non-negative".format(stdDev)
+    assert discount > 0, "discount ({0:f}) must be positive".format(discount)
+
+    d = (forward-strike) * optionType
+    if stdDev == 0:
+        return discount * max(d, 0.0)
+
+    h = d / stdDev
+    result = discount * (stdDev*_dist.derivative(h) + d*_dist(h))
+
+    assert result >= 0, "negative value ({0:f}) for " \
+                        "stdDev:  {1:f}" \
+                        "option:  {2}" \
+                        "strike:  {3:f}" \
+                        "forward: {4:f}".format(result, stdDev, optionType, strike, forward)
+    return result
+
+
+def bachelierFormulaImpliedVol(optionType,
+                               strike,
+                               forward,
+                               tte,
+                               bachelierPrice,
+                               discount=1.0):
+    strike = float(strike)
+    forward = float(forward)
+    tte = float(tte)
+    bachelierPrice = float(bachelierPrice)
+    discount = float(discount)
+
+    assert tte > 0, "tte ({0:f) must be positive".format(tte)
+    SQRT_QL_EPSILON = math.sqrt(_QL_EPSILON)
+
+    forwardPremium = bachelierPrice / discount
+
+    if optionType == OptionType.Call:
+        straddlePremium = 2.0 * forwardPremium - (forward - strike)
+    else:
+        straddlePremium = 2.0 * forwardPremium + (forward - strike)
+
+    nu = (forward - strike) / straddlePremium
+    nu = max(-1.0 + _QL_EPSILON, min(nu, 1.0 - _QL_EPSILON))
+    eta = 1.0 if (abs(nu) < SQRT_QL_EPSILON) else (nu / math.atanh(nu))
+
+    heta = HCalculator(eta)
+    impliedBpvol = math.sqrt(_M_PI / (2 * tte)) * straddlePremium * heta
+    return impliedBpvol
+
+# detail implementation
+
+
+class HCalculator(object):
+
+    _A0 = 3.994961687345134e-1
+    _A1 = 2.100960795068497e+1
+    _A2 = 4.980340217855084e+1
+    _A3 = 5.988761102690991e+2
+    _A4 = 1.848489695437094e+3
+    _A5 = 6.106322407867059e+3
+    _A6 = 2.493415285349361e+4
+    _A7 = 1.266458051348246e+4
+
+    _B0 = 1.000000000000000e+0
+    _B1 = 4.990534153589422e+1
+    _B2 = 3.093573936743112e+1
+    _B3 = 1.495105008310999e+3
+    _B4 = 1.323614537899738e+3
+    _B5 = 1.598919697679745e+4
+    _B6 = 2.392008891720782e+4
+    _B7 = 3.608817108375034e+3
+    _B8 = -2.067719486400926e+2
+    _B9 = 1.174240599306013e+1
+
+    @classmethod
+    def __call__(cls, eta):
+        num = cls._A0 + eta * (cls._A1 + eta * (cls._A2 + eta * (cls._A3 + eta
+              * (cls._A4 + eta * (cls._A5 + eta * (cls._A6 + eta * cls._A7))))))
+
+        den = cls._B0 + eta * (cls._B1 + eta * (cls._B2 + eta * (cls._B3 + eta * (cls._B4 + eta
+              * (cls._B5 + eta * (cls._B6 + eta * (cls._B7 + eta * (cls._B8 + eta * cls._B9))))))))
+
+        return math.sqrt(eta) * (num / den)
+
+
+if __name__ == "__main__":
+    pass
 
 
 

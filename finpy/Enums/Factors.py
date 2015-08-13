3246c2973fc6f299a12240b302c72c464f6ec1ba
Cheng.Li <cheng.li@datayes.com>
2015-08-13 19:54:08 +0800

    added factors enum

---
+++ finpy/Enums/Factors.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+u"""
+Created on 2015-8-13
+
+@author: cheng.li
+"""
+
+from enum import Enum
+from enum import unique
+
+
+class StrEnum(str, Enum):
+    pass
+
+
+@unique
+class Factors(StrEnum):
+
+    CLOSE = 'close'
+    PE = 'pe'
+    OPEN = 'open'
+    VOLUME = 'volume'
+    HIGH = 'high'
+    LOW = 'low'

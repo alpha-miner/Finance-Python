d677b268e2fe5452c61257a2384848db9cc37bc8
Cheng.Li <cheng.li@datayes.com>
2015-08-17 11:12:54 +0800

    chang the implementation of settings
    due to the fact that current class properties chan't work with python 3

--- finpy/Env/Settings.py
+++ finpy/Env/Settings.py
@@ -5,32 +5,30 @@ Created on 2015-8-14
 @author: cheng.li
 """
 
-from finpy.Patterns.ClassProperty import ClassPropertyMetaClass
-from finpy.Patterns.ClassProperty import classproperty
 from finpy.DateUtilities import Date
 
 
-class Settings(object):
-    __metaclass__ = ClassPropertyMetaClass
-    _evaluationDate = None
+class SettingsFactory(object):
+    def __init__(self):
+        self._evaluationDate = None
 
-    @classproperty
-    def evaluationDate(cls):
-        if cls._evaluationDate is None:
+    @property
+    def evaluationDate(self):
+        if self._evaluationDate is None:
             return Date.todaysDate()
-        return cls._evaluationDate
+        return self._evaluationDate
 
     @evaluationDate.setter
-    def evaluationDate(cls, value):
+    def evaluationDate(self, value):
         if not isinstance(value, Date):
             raise ValueError("{0} is not a valid finpy date object".format(value))
-        cls._evaluationDate = value
+        self._evaluationDate = value
 
-    @classmethod
-    def resetEvaluationDate(cls):
-        cls._evaluationDate = None
+    def resetEvaluationDate(self):
+        self._evaluationDate = None
 
-    @classmethod
-    def anchorEvaluationDate(cls):
-        if cls._evaluationDate is None:
-            cls._evaluationDate = Date.todaysDate()
+    def anchorEvaluationDate(self):
+        if self._evaluationDate is None:
+            self._evaluationDate = Date.todaysDate()
+
+Settings = SettingsFactory()

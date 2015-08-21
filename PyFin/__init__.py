621745009b38a7429252bba1c61064d4115e4aeb
Cheng.Li <cheng.li@datayes.com>
2015-08-21 10:11:31 +0800

    updated version to 0.3.3

--- PyFin/__init__.py
+++ PyFin/__init__.py
@@ -7,7 +7,7 @@ Created on 2015-7-9
 
 __all__ = ['__version__', 'API', 'DateUtilities', 'Enums', 'Env', 'Math', 'PricingEngines', 'Risk', 'AlgoTrading']
 
-__version__ = "0.3.2"
+__version__ = "0.3.3"
 
 import PyFin.API as API
 import PyFin.DateUtilities as DateUtilities

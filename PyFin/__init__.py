71190d1ae64d16df883709442ef9557f09179b95
wegamekinglc <scrappedprince.li@gmail.com>
2017-12-01 15:01:07 +0800

    bump version to 0.5.12

--- PyFin/__init__.py
+++ PyFin/__init__.py
@@ -23,4 +23,4 @@ __all__ = ['__version__',
            'Analysis',
            'Utilities']
 
-__version__ = "0.5.11"
+__version__ = "0.5.12"
--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ else:
 
 PACKAGE = "PyFin"
 NAME = "Finance-Python"
-VERSION = "0.5.11"
+VERSION = "0.5.12"
 DESCRIPTION = "PyFin " + VERSION
 AUTHOR = "cheng li"
 AUTHOR_EMAIL = "wegamekinglc@hotmail.com"

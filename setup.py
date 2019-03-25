fc59ac3ed8b8f97aeb627d2a217b05bb10303e3d
wegamekinglc <scrappedprince.li@gmail.com>
2019-03-25 09:48:52 +0800

    bump version to 0.7.1

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ else:
 
 PACKAGE = "PyFin"
 NAME = "Finance-Python"
-VERSION = "0.7.0"
+VERSION = "0.7.1"
 DESCRIPTION = "PyFin " + VERSION
 AUTHOR = "cheng li"
 AUTHOR_EMAIL = "wegamekinglc@hotmail.com"

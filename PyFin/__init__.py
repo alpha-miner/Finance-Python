99c0fd35dd7617c150484b5db9c1b42f94d494a9
wegamekinglc <scrappedprince.li@gmail.com>
2020-02-02 15:33:33 +0800

    bump version to 0.8.1

--- PyFin/__init__.py
+++ PyFin/__init__.py
@@ -23,4 +23,4 @@ __all__ = ['__version__',
            'Analysis',
            'Utilities']
 
-__version__ = "0.8.0"
+__version__ = "0.8.1"
--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ else:
 
 PACKAGE = "PyFin"
 NAME = "Finance-Python"
-VERSION = "0.8.0"
+VERSION = "0.8.1"
 DESCRIPTION = "PyFin " + VERSION
 AUTHOR = "cheng li"
 AUTHOR_EMAIL = "wegamekinglc@hotmail.com"

2887c1b78d391f64e303fa8eb1472eaaf2e603c5
wegamekinglc <scrappedprince.li@gmail.com>
2020-12-12 14:50:05 +0800

    DOC: bump version number to 0.9.1

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ else:
 
 PACKAGE = "PyFin"
 NAME = "Finance-Python"
-VERSION = "0.9.0"
+VERSION = "0.9.1"
 DESCRIPTION = "PyFin " + VERSION
 AUTHOR = "cheng li"
 AUTHOR_EMAIL = "wegamekinglc@hotmail.com"
